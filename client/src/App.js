e4aea5a28f53b25c165abe753bb73a9223145155
Praveen S Nair <praveen_772@yahoo.co.in>
2022-01-26 20:17:33 +0400

    Re Uploading

---
+++ client/src/App.js
@@ -0,0 +1,115 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [listofUsers, setlistofUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [mob, setMob] = useState(0);
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [DeleteUser, setDeleteUser] = useState("");
+
+  useEffect(() => {
+    axios.get("/").then((response) => {
+      setlistofUsers(response.data);
+    });
+  }, [listofUsers]);
+
+  const createUser = () => {
+    console.log(name);
+    console.log(mob);
+    console.log(email);
+    console.log(address);
+    axios
+      .post("/createUser", { name, mob, email, address })
+      .then((response) => {
+        setlistofUsers([...listofUsers, { name, mob, email, address }]);
+        
+        console.log("Details send to Server");
+      });
+  };
+
+  const deleteUser = () => {
+    axios
+      .post("/deleteUser", { DeleteUser })
+      .then((response) => {
+        console.log("Contact "+DeleteUser+"Deleted Successfully");
+      });
+  };
+
+  return (
+    <div className="App">
+      <div className="usersDisplay">
+        <h1 className="heading">
+          <u> Contact List </u>
+        </h1>
+
+        <div>
+          <table className="contact-table">
+            <tr>
+              <th>Name</th>
+              <th>Contact No</th>
+              <th>Email Id</th>
+              <th>Address</th>
+            </tr>
+            {listofUsers.map((user) => {
+              return (
+                <tr>
+                  <td>{user.name}</td>
+                  <td>{user.mob}</td>
+                  <td>{user.email}</td>
+                  <td>{user.address}</td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      </div>
+      <div className="createUser">
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          required
+        />
+        <input
+          type="number"
+          placeholder="Contact No..."
+          onChange={(e) => {
+            setMob(e.target.value);
+          }}
+        />
+        <input
+          type="email"
+          placeholder="Email..."
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Address..."
+          onChange={(e) => {
+            setAddress(e.target.value);
+          }}
+        />
+        <button onClick={createUser}>Create Contact</button>
+      </div>
+      <div className="deleteUser">
+        <input
+          type="text"
+          placeholder="Enter Name to delete"
+          onChange={(e) => {
+            setDeleteUser(e.target.value);
+          }}
+        />
+        <button onClick={deleteUser}>Delete Contact</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const app = express()
+const mongoClient = require('mongodb').MongoClient
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+
+db.connect((err)=>{
+    if(err) console.log("External File Database Connection Error"+err);
+    else console.log("External File Database Connected Successfully");
+})
+
+// const url = "mongodb+srv://praveen_772:praveen_772@cluster0.clivr.mongodb.net/MERN?retryWrites=true&w=majority"
+    
+app.use(cors());
+app.use(express.json());
+
+app.get("/",async(req,res)=>{
+    // const client = new mongoClient(url);
+    // await client.connect();
+    // const database = client.db("MERN");
+    const users = await db.get().collection('users').find({}).toArray()
+    res.json(users)
+})
+
+app.post("/createUser",async(req,res)=>{
+    const user = req.body;
+    // const client = new mongoClient(url);
+    // await client.connect();
+    // const database = client.db("MERN");
+    const users = await db.get().collection('users').insertOne(user);
+    console.log(user);
+})
+
+app.post("/deleteUser",async(req,res)=>{
+    const usertoDelete = req.body.DeleteUser;
+    console.log(usertoDelete);
+    // const client = new mongoClient(url);
+    // await client.connect();
+    // const database = client.db("MERN");
+    await db.get().collection('users').deleteOne({name:usertoDelete})
+    console.log("User deleted Successfully");
+})
+
+if(process.env.NODE_ENV = "production"){
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
+
+app.listen(PORT,async ()=>{
+    // const client = new mongoClient(url);
+    // await client.connect();
+    // const database = client.db("MERN")
+    const users = await db.get().collection('users').find({}).toArray()
+    console.log("Node JS Server Started on Port "+PORT);
+    console.log("Mongodb Cloud Database Connected Successfully");
+    console.log("----------------------------------------------------------");
+    console.log(users);
+    console.log("----------------------------------------------------------");
+    const user = await db.get().collection('users').findOne({name:"Praveen"})
+    console.log("----------------------------------------------------------");
+    console.log(user);
+    console.log("----------------------------------------------------------");
+    // await client.close();
+});
